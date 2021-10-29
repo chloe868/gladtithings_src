1efaa40dd6448376c885a790a3607046c5adb9f7
kennck <kennettecanales@gmail.com>
2021-10-29 14:56:12 +0800

    update dashboard

--- modules/dashboard/index.js
+++ modules/dashboard/index.js
@@ -8,6 +8,7 @@ import BalanceCard from 'modules/generic/BalanceCard.js';
 import IncrementButton from 'components/Form/Button';
 import Subscription from 'modules/generic/Subscriptions.js';
 import { Spinner } from 'components';
+import Skeleton from 'components/Loading/Skeleton';
 
 const width = Math.round(Dimensions.get('window').width)
 const height = Math.round(Dimensions.get('window').height)
@@ -33,11 +34,10 @@ class Dashboard extends Component {
   componentDidMount() {
     this.focusListener = this.props.navigation.addListener('didFocus', () => {
       this.retrieveBalance();
-      this.retrieve(false);
     })
   }
 
-  retrieve = (flag) => {
+  retrieveLedgerHistory = (flag) => {
     const { user } = this.props.state;
     let parameter = {
       condition: [{
@@ -70,30 +70,35 @@ class Dashboard extends Component {
     });
   }
 
+
   retrieveBalance = () => {
-    const { user } = this.props.state;
+    const {user} = this.props.state;
+    if (user == null) {
+      return;
+    }
     let parameter = {
-      condition: [
-        {
-          clause: '=',
-          column: 'account_id',
-          value: user.id
-        }
-      ],
       account_id: user.id,
       account_code: user.code
-    }
-    this.setState({ isLoading: true })
-    Api.request(Routes.ledgerDashboard, parameter, response => {
-      this.setState({ isLoading: false })
-      if (response.data) {
-        this.setState({ ledger: response.data.ledger[0] });
-        this.props.setLedger(response.data.ledger[0])
+    };
+    this.setState({isLoading: true});
+    Api.request(Routes.ledgerSummary, parameter, (response) => {
+      this.setState({isLoading: false});
+      const { setLedger } = this.props;
+      if (response.data.length > 0) {
+        setLedger(response.data[0])
+        this.setState({
+          ledger: response.data
+        })
+      } else {
+        setLedger(null);
+        this.setState({
+          ledger: null
+        })
       }
+      this.retrieveLedgerHistory()
     }, error => {
-      console.log(error);
-      this.setState({ isLoading: false });
-    })
+      this.setState({isLoading: false, history: null});
+    });
   }
 
   render() {
@@ -114,10 +119,16 @@ class Dashboard extends Component {
             paddingRight: 15,
             height: height * 1.5,
           }}>
-            {isLoading ? <Spinner mode="overlay" /> : null}
+          
             {
-              ledger && (
-                <BalanceCard data={ledger} />
+              (this.state.ledger && this.state.ledger.length > 0) && this.state.ledger.map((item, index) => (
+                <BalanceCard data={item} style={{marginTop: 20}}/>
+              ))
+            }
+            
+            {
+              (isLoading) && (
+                <Skeleton size={1} template={'block'} height={125}/>
               )
             }
 
