044c8db7923fd731b28491afab751b3df2b31d83
kennck <kennettecanales@gmail.com>
2022-01-19 15:46:55 +0800

    update

---
+++ common/Countries.js
@@ -0,0 +1,21 @@
+export default{
+  list: [{
+    title: 'Philippines',
+    currency: 'PHP',
+    currencyLabel: 'Philippine PESO',
+    mobilePrefix: '+63',
+    code: 'PH'
+  }, {
+    title: 'United States',
+    currency: 'USD',
+    currencyLabel: 'US DOLLAR',
+    mobilePrefix: '+1',
+    code: 'US'
+  }, {
+    title: 'Thailand',
+    currency: 'THB',
+    currencyLabel: 'THAILAND BAHT',
+    mobilePrefix: '+1',
+    code: 'TH'
+  }]
+}
--- services/Currency.js
+++ services/Currency.js
@@ -1,5 +1,6 @@
 require('intl');
 require('intl/locale-data/jsonp/en.js');
+import Countries from '../common/Countries';
 export default {
   display(amount, currency){
     var formatter = new Intl.NumberFormat('en-US', {
@@ -11,5 +12,14 @@ export default {
   getMonth(index){
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     return months[index];
+  },
+  getWithCountry(currency){
+    country = Countries.list.filter((item) => {
+      return item.currency == currency;
+    })
+    if(country){
+      return country[0].currencyLabel + ` - ${country[0].currency}`
+    }
+    return null
   }
 }
